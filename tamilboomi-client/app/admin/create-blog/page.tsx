d6e3f85d95a571114f2a69fe60259b27d7b770bb
SATHESH P C <110752666+sathesh-777@users.noreply.github.com>
2024-01-16 00:13:10 +0600

    Update page.tsx

--- tamilboomi-client/app/admin/create-blog/page.tsx
+++ tamilboomi-client/app/admin/create-blog/page.tsx
@@ -1,28 +1,33 @@
-'use client'
-import DashboardHeader from '@/app/components/Admin/DashboardHeader'
-import Heading from '@/app/utils/Heading'
+"use client";
+import DashboardHeader from "@/app/components/Admin/DashboardHeader";
+import Heading from "@/app/utils/Heading";
 import AdminSidebar from "../../components/Admin/sidebar/AdminSidebar";
-import UploadBlog from "@/app/components/Blog/UploadBlog";
+import dynamic from "next/dynamic";
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import("@/app/components/Blog/UploadBlog"),
+  { ssr: false }
+);
 
 const Page = () => {
   return (
-    <div className='bg-[#010100]'>
-        <Heading
-         title="Elearning - Admin"
-         description="ELearning is a platform for students to learn and get help from teachers"
-         keywords="Prograaming,MERN,Redux,Machine Learning"
-        />
-        <div className="flex">
-            <div className="1500px:w-[16%] w-1/5">
-                <AdminSidebar />
-            </div>
-            <div className="w-[85%]">
-               <DashboardHeader />
-               <UploadBlog /> 
-            </div>
+    <div className="bg-[#010100]">
+      <Heading
+        title="Elearning - Admin"
+        description="ELearning is a platform for students to learn and get help from teachers"
+        keywords="Prograaming,MERN,Redux,Machine Learning"
+      />
+      <div className="flex">
+        <div className="1500px:w-[16%] w-1/5">
+          <AdminSidebar />
         </div>
+        <div className="w-[85%]">
+          <DashboardHeader />
+          <DynamicComponentWithNoSSR />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
