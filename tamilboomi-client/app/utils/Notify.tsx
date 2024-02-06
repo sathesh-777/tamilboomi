c36829dc902c7a94625004240662232821881129
sathesh-777 <p.c.sathesh777@gmail.com>
2024-02-06 19:27:33 +0530

    changes triggerup academy stuffs and issues 2

--- tamilboomi-client/app/utils/Notify.tsx
+++ tamilboomi-client/app/utils/Notify.tsx
@@ -15,17 +15,17 @@ const Notify: FC<NotifyProps> = ({ text }) => {
   return (
     <>
       {isOpen && (
-        <div className="w-full text-center bg-[#397CFD] py-2 md:text-md text-[12px]">
+        <div className="w-full text-center bg-[#397CFD] py-2 ">
           <Link href={"https://chat.whatsapp.com/Dtt7HHvfBvvAzXYU8z4cWX"}>
             <span
-              className={` font-Poppins font-[500] text-center text-white`}
+              className={`md:text-lg text-[12px] font-Poppins font-[500] text-center text-white `}
             >
               {text}  <b className="underline">join our Whatsapp Teach community</b>
             </span>
           </Link>
           <button
             onClick={closeNotification}
-            className="text-md cursor-pointer absolute right-2"
+            className="md:text-lg text-[12px] cursor-pointer absolute right-2"
           >
             &#10006; {/* This is the close (X) icon */}
           </button>
